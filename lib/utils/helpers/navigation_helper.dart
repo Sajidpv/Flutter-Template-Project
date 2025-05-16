27f5f988cd68c4e5c62a271132fc7688c8bc2be6
SAJID PV <125041012+Sajidpv@users.noreply.github.com>
2025-05-16 15:40:05 +0530

    Update navigation_helper.dart

--- lib/utils/helpers/navigation_helper.dart
+++ lib/utils/helpers/navigation_helper.dart
@@ -3,23 +3,32 @@ import 'package:erp/cors/configs/enums.dart';
 import 'package:erp/routes/route_names.dart';
 
 void navigateUserByRole(BuildContext context, UserRole? role) {
-  if (role == UserRole.admin || role == UserRole.subAdmin) {
-    Navigator.pushNamedAndRemoveUntil(
-      context,
-      RoutesName.admin_home,
-      (route) => false,
-    );
-  } else if (role == UserRole.user) {
-    Navigator.pushNamedAndRemoveUntil(
-      context,
-      RoutesName.user_home,
-      (route) => false,
-    );
-  } else {
-    Navigator.pushNamedAndRemoveUntil(
-      context,
-      RoutesName.auth,
-      (route) => false,
-    );
+  Navigator.pushNamedAndRemoveUntil(
+    context,
+    getHomeScreen(role),
+    (route) => false,
+  );
+}
+
+String getHomeScreen(UserRole? role) {
+  switch (role) {
+    case UserRole.admin:
+      return RoutesName.admin_home;
+    case UserRole.supervisor:
+      return RoutesName.supervisor_home;
+    case UserRole.master:
+      return RoutesName.master_home;
+    case UserRole.tailor:
+      return RoutesName.tailor_home;
+    case UserRole.button:
+      return RoutesName.button_home;
+    case UserRole.threading:
+      return RoutesName.threading_home;
+    case UserRole.qc:
+      return RoutesName.qc_home;
+    case UserRole.customer:
+      return RoutesName.customer_home;
+    default:
+      return RoutesName.login;
   }
 }
